63ae7cc22eaf86cc3d1b1d2372cb8ea462d73dde
Siddhant Saxena <sidd.co.in@gmail.com>
2018-03-28 00:17:15 +0530

    Initial Commit

---
+++ SSSideMenu/SSSideMenu.h
@@ -0,0 +1,19 @@
+//
+//  SSSideMenu.h
+//  SSSideMenu
+//
+//  Created by Siddhant Saxena on 28/03/18.
+//  Copyright © 2018 Siddhant Saxena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSSideMenu.
+FOUNDATION_EXPORT double SSSideMenuVersionNumber;
+
+//! Project version string for SSSideMenu.
+FOUNDATION_EXPORT const unsigned char SSSideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSSideMenu/PublicHeader.h>
+
+
